7e990bfa437efd1362b453ce08d158b40e68c370
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-02-07 22:28:24 -0500

    Initial Commit

---
+++ Assets/_Scripts/OceanController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanController : MonoBehaviour
+{
+    public float verticalSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    void Move() {
+        float currentY = transform.position.y;
+        transform.position = new Vector2(0.0f, currentY - verticalSpeed);
+    }
+
+    void Reset() {
+        transform.position = new Vector2(0.0f, 2.2f);
+    }
+
+    void CheckBounds() {
+        if(transform.position.y < -2.2) {
+            Reset();
+        }
+    }
+}
